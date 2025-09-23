6a7b6b947ff2e7b4346f4c50056730b2dae942b7
SarahNhl <sarah.nahal@kpconsulting.fr>
2025-09-23 15:39:26 +0200

    Update streamlit_app (1).py

--- streamlit_app (1).py
+++ streamlit_app (1).py
@@ -23,8 +23,8 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 #st.stop()
 
 pd_df= my_dataframe.to_pandas()
-st.my_dataframe(pd_df)
-st.stop
+#st.my_dataframe(pd_df)
+#st.stop
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:'
