dcfd9f56288c5158ca7b65d6249c9562c82d3a3a
SarahNhl <sarah.nahal@kpconsulting.fr>
2025-09-23 16:15:16 +0200

    Update streamlit_app (1).py

--- streamlit_app (1).py
+++ streamlit_app (1).py
@@ -47,10 +47,10 @@ if ingredients_list:
     
     #st.write(ingredients_string)
 
-isOrderFilled= st.text_input("True")
+#isOrderFilled= st.text_input("True")
   
-    my_insert_stmt = """ insert into smoothies.public.orders(ingredients,NAME_ON_ORDER,ORDER_FILLED)
-        values ('""" + ingredients_string + """','""" + name_on_order + """ + isOrderFilled +"""')"""
+    my_insert_stmt = """ insert into smoothies.public.orders(ingredients,NAME_ON_ORDER)
+        values ('""" + ingredients_string + """','""" + name_on_order + """')"""
 
     st.write(my_insert_stmt)
     #st.stop()
