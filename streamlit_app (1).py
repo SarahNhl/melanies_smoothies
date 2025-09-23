cd6fc8e9d4585fb3e31ef9a9ef8da1b8341cb93d
SarahNhl <sarah.nahal@kpconsulting.fr>
2025-09-23 15:37:11 +0200

    Update streamlit_app (1).py

--- streamlit_app (1).py
+++ streamlit_app (1).py
@@ -3,7 +3,7 @@ import streamlit as st
 from snowflake.snowpark.functions import col
 
 import requests
-import pandas
+import pandas as pd
 
 # Write directly to the app
 st.title("Customize Your Smoothie! :cocktail:")
