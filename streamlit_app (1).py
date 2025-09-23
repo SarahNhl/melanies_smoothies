4873f4cb9bae8077937098387889a96fb8e0296a
SarahNhl <sarah.nahal@kpconsulting.fr>
2025-09-23 13:59:28 +0200

    Update streamlit_app (1).py

--- streamlit_app (1).py
+++ streamlit_app (1).py
@@ -3,7 +3,7 @@ import streamlit as st
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
-st.title(f"Customize Your Smoothie! :balloon: ")
+st.title(f"Customize Your Smoothie! :smoothie: ")
 st.write(
   """Choose the fruits you want in your custom Smoothie!
   """
